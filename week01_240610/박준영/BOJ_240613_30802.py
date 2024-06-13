b100391dbfdd68a5a75f0523b729eaa1227a5b4e
박준영 <psw9502@naver.com>
2024-06-13 17:43:31 +0900

    박준영_240613_30802

---
+++ week01_240610/박준영/BOJ_240613_30802.py
@@ -0,0 +1,17 @@
+import sys
+
+N = int(sys.stdin.readline())
+size = list(map(int, sys.stdin.readline().split()))
+T, P = map(int, sys.stdin.readline().split())
+
+T_num = 0
+P_sum, P_num = divmod(N, P)
+
+for i in size:
+       a, b = divmod(i, T)
+       T_num += a
+       if b != 0:
+              T_num += 1
+
+print(T_num)
+print(P_sum, P_num)
