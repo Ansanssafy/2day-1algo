b3d2e387b5e9cc38544c35883bb380dba9327abb
JYPJUN <psw9502@naver.com>
2024-06-19 11:41:55 +0900

    박준영_240619_16724

---
+++ week02_240617/박준영/BOJ_240619_16724.py
@@ -0,0 +1,47 @@
+N, M = map(int, input().split())
+
+arr = [list(input()) for _ in range(N)]
+
+parent = [i for i in range(M*N)]
+
+dict = {
+    'D': (1, 0),
+    'U': (-1, 0),
+    'L': (0, -1),
+    'R': (0, 1),
+}
+
+def getParent(num):
+    if num == parent[num]:
+        return num
+    else:
+        parent[num] = getParent(parent[num])
+        return parent[num]
+
+def unionParent(x, y):
+    x_root = getParent(x)
+    y_root = getParent(y)
+    if x_root != y_root:
+        if x_root < y_root:
+            parent[y_root] = x_root
+        else:
+            parent[x_root] = y_root
+    return
+
+for num in range(M*N):
+    x = num % M
+    y = num // M
+    move = dict[arr[y][x]]
+    a, b = x + move[1], y + move[0]
+    next_move = b*M + a
+    if next_move < 0 or next_move >= N*M:
+        continue
+    unionParent(num, next_move)
+
+answer = 0
+visited = set()
+for i in range(N*M):
+    if getParent(parent[i]) not in visited:
+        answer += 1
+        visited.add(parent[i])
+print(answer)
