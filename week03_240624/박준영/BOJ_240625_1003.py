fc3a0cae6bc6ccac0a5d1acfc6633850a272ed1f
JYPJUN <psw9502@naver.com>
2024-06-25 09:56:07 +0900

    박준영_1003

---
+++ week03_240624/박준영/BOJ_240625_1003.py
@@ -0,0 +1,24 @@
+def fibo(N):
+    dp = [(0, 0)] * (N+1)
+    
+    if N == 0:
+        return (1, 0)
+    elif N == 1:
+        return (0, 1)
+    
+    dp[0] = (1, 0)
+    dp[1] = (0, 1)
+    
+    for i in range(2, N+1):
+        dp[i] = (dp[i-1][0] + dp[i-2][0], dp[i-1][1] + dp[i-2][1])
+    
+    return dp[N]
+
+T = int(input())
+
+for t in range(T):
+    N = int(input())
+    
+    values = fibo(N)
+    
+    print(values[0], values[1])
