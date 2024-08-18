ddacd27eb2db98e8eac5fbe16ec473d74e932f53
kinterlocked <dusehd0525@naver.com>
2024-08-19 02:19:55 +0900

    김연동_240818_7785

---
+++ week10_240812/김연동/BOJ_240818_7785.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_240818_7785 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader  br =  new BufferedReader (new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+        }
+    }
+}
