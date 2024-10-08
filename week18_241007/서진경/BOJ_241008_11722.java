3e83131813a3c0dc02912ca4432292f73bbbbedd
JinKyeong-Seo <sjk2700@naver.com>
2024-10-08 16:09:32 +0900

    서진경_241008_11722

---
+++ week18_241007/서진경/BOJ_241008_11722.java
@@ -0,0 +1,27 @@
+package silver;
+
+import java.io.*;
+import java.util.*;
+
+public class boj_11722_가장긴감소하는부분수열 {
+    static BufferedReader br;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        int[][] arr = new int[2][N];
+
+        int res = 0;
+        for(int i=0; i<N; i++){
+            arr[0][i] = Integer.parseInt(st.nextToken());
+            arr[1][i] = 1;
+            for(int j=i; j>=0; j--){
+                if(arr[0][j]>arr[0][i]) arr[1][i] = Math.max(arr[1][i], arr[1][j]+1);
+            }
+            res = Math.max(res, arr[1][i]);
+        }
+        System.out.println(res);
+    }
+}
