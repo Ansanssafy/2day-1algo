c986cbf5a1fbe0a870d7d09062336ab897aa8153
JYPJUN <psw9502@naver.com>
2024-11-18 01:56:23 +0900

    박준영_241117_11286_11403

---
+++ week23_241111/박준영/BOJ_241117_11286.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    x = int(input())
+    if x != 0:
+        heapq.heappush(heap, (abs(x), x))
+    else:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
