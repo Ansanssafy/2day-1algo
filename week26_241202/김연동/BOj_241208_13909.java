b2b5e1a65b651195052c695f98f5f28089bc481c
kinterlocked <dusehd0525@naver.com>
2024-12-09 21:56:09 +0900

    김연동_241208_13909

---
+++ week26_241202/김연동/BOj_241208_13909.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOj_241208_13909 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for(int i = 1; i * i <= N; i++){
+            count++;
+        }
+        System.out.println(count);
+    }
+}
