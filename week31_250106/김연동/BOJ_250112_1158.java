e0c98c69f07dbe620cb909e4c92269aab6448762
kinterlocked <dusehd0525@naver.com>
2025-01-13 00:52:39 +0900

    김연동_250112_1158

---
+++ week31_250106/김연동/BOJ_250112_1158.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class BOJ_250112_1158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> q = new ArrayDeque<>();
+
+        for(int i=1; i<=N; i++) {
+            q.offerLast(i);
+        }
+
+        sb.append("<");
+        while(!q.isEmpty()){
+            for(int i=0; i<K-1; i++){
+                q.offerLast(q.pollFirst());
+            }
+            sb.append(q.pollFirst());
+            if(!q.isEmpty()) {
+                sb.append(", ");
+            }
+        }
+        sb.append(">");
+        System.out.println(sb.toString());
+    }
+}
