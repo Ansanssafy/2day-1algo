acccf3d90419be3913dba4f24008fe696681865a
JinKyeong-Seo <sjk401930@gmail.com>
2025-01-19 03:22:10 +0900

    서진경_250118_18353

---
+++ week32_250113/서진경/BOJ_250118_18353.java
@@ -0,0 +1,29 @@
+package Beakjoon.silver;
+
+import java.io.*;
+import java.util.*;
+
+public class boj_18353_병사배치하기 {
+    static BufferedReader br;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        int[][] arr = new int[2][N];
+        int max = 0;
+        for(int i=0; i<N; i++){
+            arr[0][i] = Integer.parseInt(st.nextToken());
+            arr[1][i] = 1;
+            for(int j=i-1; j>=0; j--){
+                if(arr[0][j] > arr[0][i]) {
+                    arr[1][i] = Math.max(arr[1][i], arr[1][j]+1);
+                }
+            }
+            max = Math.max(max, arr[1][i]);
+        }
+
+        System.out.println(N-max);
+    }
+}
