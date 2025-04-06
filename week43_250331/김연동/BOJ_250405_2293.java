e01f6522eac270b50df672198baa02e9be98dc8b
kinterlocked <dusehd0525@naver.com>
2025-04-07 01:58:20 +0900

    김연동_250405_2293

---
+++ week43_250331/김연동/BOJ_250405_2293.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_250405_2293 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[k + 1];
+        int[] arr = new int[n + 1];
+        for(int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = arr[i]; j <= k; j++) {
+                dp[j] += dp[j - arr[i]];
+            }
+        }
+        System.out.println(dp[k]);
+    }
+}
