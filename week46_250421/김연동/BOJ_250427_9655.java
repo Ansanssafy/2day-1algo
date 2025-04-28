98820c509307d2fcbd82324b9e658f2b995a2a34
kinterlocked <dusehd0525@naver.com>
2025-04-28 18:38:48 +0900

    김연동_250427_9655

---
+++ week46_250421/김연동/BOJ_250427_9655.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_250427_9655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N % 2 == 0) {
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+    }
+}
