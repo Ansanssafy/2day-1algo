64e6d278229de8696216c4c65c6492ad5573bfee
kinterlocked <dusehd0525@naver.com>
2025-05-22 23:38:50 +0900

    김연동_250518_1138

---
+++ week49_250512/김연동/BOJ_250518_1138.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_250518_1138 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+    }
+}
